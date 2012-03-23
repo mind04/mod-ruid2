5c9740dc3e8a2d9d066363cd5e15583e47bf9a37
Kees Monshouwer <mind04@monshouwer.org>
2012-03-23 17:30:50 +0000

    change explanation RMode option
    git-svn-id: https://mod-ruid.svn.sourceforge.net/svnroot/mod-ruid@36 541e799a-c857-49a9-9955-d080b115f20d

--- mod_ruid2.c
+++ mod_ruid2.c
@@ -289,7 +289,7 @@ static const char *set_documentchroot (cmd_parms *cmd, void *mconfig, const char
 /* configure options in httpd.conf */
 static const command_rec ruid_cmds[] = {
 
-	AP_INIT_TAKE1 ("RMode", set_mode, NULL, RSRC_CONF | ACCESS_CONF, "stat or config (default stat)"),
+	AP_INIT_TAKE1 ("RMode", set_mode, NULL, RSRC_CONF | ACCESS_CONF, "Set mode to config or stat (default: config)"),
 	AP_INIT_TAKE2 ("RUidGid", set_uidgid, NULL, RSRC_CONF | ACCESS_CONF, "Minimal uid or gid file/dir, else set[ug]id to default (User,Group)"),
 	AP_INIT_ITERATE ("RGroups", set_groups, NULL, RSRC_CONF | ACCESS_CONF, "Set aditional groups"),
 	AP_INIT_TAKE2 ("RDefaultUidGid", set_defuidgid, NULL, RSRC_CONF, "If uid or gid is < than RMinUidGid set[ug]id to this uid gid"),
